bab9c3009dc3b63099f8d6c51564be21d0b0bbc3
tanji1804 <ronkinnatsu@icloud.com>
2023-03-02 13:20:07 +0000

    laravel10 done

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class NwsController extends Controller
+class NewsController extends Controller
 {
     //
     public function add()
