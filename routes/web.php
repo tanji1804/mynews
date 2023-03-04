72e1557dfe7f0404cf439284a293bc86e637e682
tanji1804 <ronkinnatsu@icloud.com>
2023-03-04 13:46:21 +0000

    laravel13 task done

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,12 @@ Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middl
 });
 
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->prefix('admin')->group(function(){
-    Route::get('profile/create', 'add')->middleware('auth');
-    Route::get('profile/edit', 'edit')->middleware('auth');
+Route::controller(ProfileController::class)->prefix('admin')->middleware('auth')->group(function(){
+    Route::get('profile/create', 'add')->name('profile.add');
+    Route::post('profile/create', 'create')->name('profile.create');
+    
+    Route::get('profile/edit', 'edit')->name('profile.edit');
+    Route::post('profile/edit', 'update')->name('profile.update');
 });
 Auth::routes();
 
