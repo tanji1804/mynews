78301b8ec19a844876b7595b06dcd756fcd66a3e
tanji1804 <ronkinnatsu@icloud.com>
2023-03-04 12:32:05 +0000

    laravel13 done

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -7,9 +7,13 @@ use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
-    //
     public function add()
     {
-    return view('admin.news.create');    
+        return view('admin.news.create');    
+    }
+    
+    public function create(Request $request)
+    {
+        return redirect('admin/news/create');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,9 @@ Route::get('/', function () {
 });
 
 use App\Http\Controllers\Admin\NewsController;
-Route::controller(NewsController::class)->prefix('admin')->group(function(){
-    Route::get('news/create', 'add')->middleware('auth');
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function(){
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/ceate', 'create')->name('news.create');
 });
 
 use App\Http\Controllers\Admin\ProfileController;
